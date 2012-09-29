c63424d7f1f31f007062606d81fbd36f86dcdc6f
Dorian JAMINAIS <dorian@jaminais.fr>
2012-09-29 15:55:08 -0300

    implemented default layout + login layout + started the main screen added a lot of icons

--- app.js
+++ app.js
@@ -46,8 +46,12 @@ app.configure(function(){
 	app.use(express.cookieParser());
 	app.use(express.session({ secret: nconf.get("secret")}));
 	app.use(app.router);
+	app.set('view engine', 'jade');
+	app.set('view options', { layout: false });
+	app.set('views', nconf.get('basedir') + '/views');
 	app.use(express.static(nconf.get('basedir') +'/client'));
 	app.use('/template', express.static(nconf.get('basedir') +'/templates'));
+	app.use('/static', express.static(nconf.get('basedir') +'/static'));
 });
 
 app.configure('development', function(){
@@ -74,7 +78,25 @@ app.post('/new/presentation',verifiedUser, routes.presentation.newPresentation);
 app.get('/list/presentations', verifiedUser, routes.presentation.getList);
 app.get('/list/templates', routes.template.getList);
 
-app.get('/test/:schema', routes.presentation.test);
+app.get('/test', function(rer, res){res.render('splash')});
+app.get('/test/list/presentation', function(rer, res){res.render('home', 
+	{ presentations: [
+		{	id: "presentationID1", 
+			title: "Title of the presentation 1",
+			templateUrl: "/template/test1.css",
+			lastEdit: "Thu Sep 27 2012", 
+			firstSlide: "<h1>Title 1</h1><p>test</p>"},
+		{	id: "presentationID2", 
+			title: "Title of the presentation 2",
+			templateUrl: "/template/test2.css",
+			lastEdit: "Thu Sep 27 2012", 
+			firstSlide: "<h1>Title 2</h1><p>test</p>"},
+		{	id: "presentationID3", 
+			title: "Title of the presentation 3",
+			templateUrl: "/template/test3.css",
+			lastEdit: "Thu Sep 27 2012", 
+			firstSlide: "<h1>Title 3</h1><p>test</p>"},
+]});});
 
 app.listen(3000); // FIXME : use nconf 
 console.log("Express server listening on port %d in %s mode", 
